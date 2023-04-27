adc6535534c8719680df9f2f4de416a90aa03df4
Tyler McCarthy <tylerrmccarthy0@gmail.com>
2023-04-27 11:14:28 +0100

    Update .DS_Store

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,1 +1,31 @@
 #data loader for  dataset
+
+import os
+import csv
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+class DataLoader(keras.utils.Sequence):
+    def __init__(self, data_dir, target_size=(32,32), batch_size=32, uids=None):
+        self.data_dir = data_dir
+        self.target_size = target_size
+        self.batch_size = batch_size
+        self.uids = uids
+        self.image_paths = self.get_image_paths()
+        self.num_images = len(self.image)
+        self.indexes = np.arrange(self.num_images)
+
+        if self.uids is None and self.uid_path is None:
+            raise ValueError("Either uids or uid_path must be provided")
+        
+        if self.uids is None:
+            self.uids = self._load_uids_from_csv()
+
+        if len(self.uids)!= self.num_images:
+            raise ValueError("Number of uids doesn't match number of images")
+
+    def __len__(self):
+        return  int(np.ceil(self.num_images/self.batch_size))
+    
+    #def __getitem__(self, idx):
--- utils/model.py
+++ utils/model.py
@@ -1,1 +1,14 @@
 #model architecture
+import tensorflow as tf
+from tensorflow import keras
+
+# using keras create a CNN
+inputs = keras.Input(shape=(None, None, 3))
+processed = keras.layers.RandomCrop(width=32, height=32)(inputs)
+conv = keras.layers.Conv2D(filters=2, kernel_size=3)(processed)
+pooling = keras.layers.GlobalAveragePooling2D()(conv)
+feature = keras.layers.Dense(10)(pooling)
+
+full_model = keras.Model(inputs, feature)
+backbone = keras.Model(processed, conv)
+activations = keras.Model(conv, feature)
